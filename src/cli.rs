da1b029c6e292c379dfb53b2bad7018421300008
Jason White <jasonaw0@gmail.com>
2017-02-17 02:32:10 -0800

    Add initial argument parsing framework

---
+++ src/cli.rs
@@ -0,0 +1,168 @@
+// Copyright (c) 2017 Jason White
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+use std::path::PathBuf;
+use std::str::FromStr;
+
+use clap;
+
+/// Returns clap App arguments to use for parsing.
+pub fn app<'a, 'b>() -> clap::App<'a, 'b> {
+
+    // Common options
+    let verbose_opt = clap::Arg::with_name("verbose")
+        .help("Print additional information.")
+        .long("verbose")
+        .short("v");
+
+    let file_opt = clap::Arg::with_name("file")
+        .help("Path to the build description.")
+        .takes_value(true)
+        .long("file")
+        .short("f");
+
+    let dryrun_opt = clap::Arg::with_name("dryrun")
+        .help("Print what might happen.")
+        .long("dryrun")
+        .short("n");
+
+    let threads_opt = clap::Arg::with_name("threads")
+        .help("The number of threads to use.")
+        .takes_value(true)
+        .long("threads")
+        .short("t")
+        .default_value("8");
+
+    let color_opt = clap::Arg::with_name("color")
+        .help("When to colorize the output.")
+        .takes_value(true)
+        .long("color")
+        .possible_values(&["auto", "never", "always"])
+        .default_value("auto");
+
+    let common_opts = &[verbose_opt, file_opt, dryrun_opt, threads_opt, color_opt];
+
+    clap::App::new("button")
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .setting(clap::AppSettings::VersionlessSubcommands)
+        .subcommand(clap::SubCommand::with_name("build")
+            .about("Builds your damn software.")
+            .alias("update")
+            .args(common_opts)
+            .args(&[
+                clap::Arg::with_name("auto")
+                    .help("Wait for changes and build automatically.")
+                    .long("auto"),
+
+                clap::Arg::with_name("watchdir")
+                    .help("Used with --auto. The directory to watch for changes in.")
+                    .takes_value(true)
+                    .long("watchdir"),
+
+                clap::Arg::with_name("delay")
+                    .help("Used with --auto. The number of milliseconds to wait before \
+                          building.")
+                    .takes_value(true)
+                    .long("delay")
+                    .default_value("50"),
+            ])
+        )
+        .subcommand(clap::SubCommand::with_name("clean")
+            .about("Cleans your damn software.")
+            .args(common_opts)
+            .args(&[
+                clap::Arg::with_name("purge")
+                    .help("Delete the build state too.")
+                    .long("purge"),
+            ])
+        )
+}
+
+#[derive(Debug)]
+pub enum Coloring {
+    Auto,
+    Never,
+    Always
+}
+
+impl FromStr for Coloring {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "auto"   => Ok(Coloring::Auto),
+            "never"  => Ok(Coloring::Never),
+            "always" => Ok(Coloring::Always),
+            _        => Err("no match")
+        }
+    }
+}
+
+/// 'build' subcommand options
+#[derive(Debug)]
+pub struct BuildOpts {
+    pub file: Option<PathBuf>,
+    pub dryrun: bool,
+    pub color: Coloring,
+    pub threads: usize,
+    pub autobuild: bool,
+    pub delay: usize,
+}
+
+/// 'clean' subcommand options
+#[derive(Debug)]
+pub struct CleanOpts {
+    pub file: Option<PathBuf>,
+    pub dryrun: bool,
+    pub color: Coloring,
+    pub threads: usize,
+    pub purge: bool,
+}
+
+pub enum Command {
+    Build(BuildOpts),
+    Clean(CleanOpts),
+}
+
+pub fn subcommand<'a>(name: &str, matches: &clap::ArgMatches<'a>) -> clap::Result<Command> {
+    match name {
+        "build" => Ok(Command::Build(BuildOpts {
+            file: matches.value_of("file").map(|f| PathBuf::from(f)),
+            dryrun: matches.is_present("dryrun"),
+            color: try!(value_t!(matches.value_of("color"), Coloring)),
+            threads: try!(value_t!(matches, "threads", usize)),
+            autobuild: matches.is_present("auto"),
+            delay: try!(value_t!(matches, "delay", usize)),
+        })),
+
+        "clean" => Ok(Command::Clean(CleanOpts {
+            file: matches.value_of("file").map(|f| PathBuf::from(f)),
+            dryrun: matches.is_present("dryrun"),
+            color: try!(value_t!(matches.value_of("color"), Coloring)),
+            threads: try!(value_t!(matches, "threads", usize)),
+            purge: matches.is_present("purge"),
+        })),
+
+        // If all subcommands are matched above, then this shouldn't happen.
+        _ => unreachable!(),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+// Copyright (c) 2017 Jason White
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+#[macro_use] extern crate clap;
+
+mod cli;
+
 fn main() {
+
+    let app_matches = cli::app().get_matches();
+
+    let (name, matches) = app_matches.subcommand();
+
+    if let Some(matches) = matches {
+        match cli::subcommand(name, matches) {
+            Ok(cli::Command::Build(opts)) => {
+                println!("{:?}", opts);
+            },
+            Ok(cli::Command::Clean(opts)) => {
+                println!("{:?}", opts);
+            },
+            Err(err) => {
+                println!("{}", err);
+            },
+        };
+    }
+
     println!("Hello, world!");
 }
